2235d8a2ff02d327c3bd7bb3f3f7456700e9d8e4
Samuel <mastercraft9090@gmail.com>
2021-02-12 16:13:13 -0500

    first shelk script for random password generation

---
+++ luser-demo05.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#This scripts generates a list of random passwords.
+
+# A random number as a password
+
+PASSWORD="${RANDOM}" #Variable Password is assigned to RANDOM which is an in-built in bash funciton for generating random numbers.
+echo "${PASSWORD}"
