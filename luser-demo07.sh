7e386e57ea5be7303f288c90b6f53fd266611a2d
Samuel <mastercraft9090@gmail.com>
2021-02-12 21:08:28 -0500

    new code

---
+++ luser-demo07.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Demonstrate the use of the shift and while loops
+
+
+# Display the first three parameters
+
+echo "Parameter 1: ${1}"
+echo "Parameter 2: ${2}"
+echo "Parameter 3: ${3}"
+
+echo
+
+# Loop through all the positional parameters
+while [[ "${#} -gt 0 ]]
+do
+  echo "Number of Parameters: ${#}"
+  echo "Paramter 1: ${1}"
+  echo "Paramter 2: ${2}"
+  echo "Paramter 3: ${3}"
+  echo
+  shift
+done
