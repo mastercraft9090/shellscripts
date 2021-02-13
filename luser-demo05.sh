d351c6cce8e26ebe758b90d811fbb92a62a10a7e
Samuel <mastercraft9090@gmail.com>
2021-02-12 19:55:41 -0500

    new code

--- luser-demo05.sh
+++ luser-demo05.sh
@@ -4,5 +4,33 @@
 
 # A random number as a password
 
-PASSWORD="${RANDOM}" #Variable Password is assigned to RANDOM which is an in-built in bash funciton for generating random numbers.
+PASSWORD="${RANDOM}"
 echo "${PASSWORD}"
+
+PASSWORD="${RANDOM}${RANDOM}${RANDOM}" #Variable Password is assigned to RANDOM which is an in-built in bash funciton for generating random numbers.
+echo "${PASSWORD}"
+
+# Use the current date/time as a basis for the passwd
+
+PASSWORD=$(date +%s) # This assign this to the variable/
+echo "${PASSWORD}"
+
+
+# Use nano seconds to act as randomization
+PASSWORD=$(date +%s%N)
+echo "${PASSWORD}"
+
+# Create a better a better password
+PASSWORD=$(date +%s%N) | sha256sum | head -c15
+echo "${PASSWORD}"
+
+PASSWORD=$(date +%s%N | sha256sum | head -c32)
+echo "${PASSWORD}"
+
+#An even better password
+PASSWORD=$(date +%s%N${RANDOM}${RANDOM} | sha256sum | head -c48)
+echo "${PASSWORD}"
+
+# Append a special character to the string
+SPECIAL_CHARACTER=$(echo '!@#$%^&*' | fold -w1 | shuf | head -c1)
+echo "${PASSWORD}${SPECIAL_CHARACTER}"
